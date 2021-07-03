13c146b8e2d25511138449f0646018f59c3bfbf7
nSystemz <info@nsystemz.de>
2021-07-03 15:30:11 +0200

    Part #11

--- client_packages/login/login.js
+++ client_packages/login/login.js
@@ -1,42 +1,39 @@
-const Browser = require('./classes/browser');
-
-let loginBrowser = new Browser('loginBrowser', 'package://web/login/login.html');
+let loginWindow = mp.browsers.new("package://web/login/login.html");
 
 setTimeout(() => {
     mp.gui.cursor.show(true, true);
     mp.game.ui.displayHud(false);
     mp.gui.chat.show(false);
     mp.game.ui.displayRadar(false);
-}, 250)
+}, 150)
 
-mp.events.add({
-    "Auth.Register": (password) => {
-        mp.gui.cursor.show(false, false);
-        mp.gui.chat.show(true);
-        mp.game.ui.displayHud(true);
-        mp.game.ui.displayRadar(true);
+mp.events.add("Auth.Register", (password) => 
+{
+    mp.gui.cursor.show(false, false);
+    mp.gui.chat.show(true);
+    mp.game.ui.displayHud(true);
+    mp.game.ui.displayRadar(true);
 
-        mp.events.callRemote('Auth.OnRegister', password);
+    mp.events.callRemote('Auth.OnRegister', password);
 
-        if(loginBrowser !== null)
-        {
-            loginBrowser.close();
-            loginBrowser = null;
-        }
-    },
+    if(loginWindow != null)
+    {
+        loginWindow.destroy();
+        loginWindow = null;
+    }
+})
 
-    "Auth.Login": (password) => {
-        mp.gui.cursor.show(false, false);
-        mp.gui.chat.show(true);
-        mp.game.ui.displayHud(true);
-        mp.game.ui.displayRadar(true);
+mp.events.add("Auth.Login", (password) => {
+    mp.gui.cursor.show(false, false);
+    mp.gui.chat.show(true);
+    mp.game.ui.displayHud(true);
+    mp.game.ui.displayRadar(true);
 
-        mp.events.callRemote('Auth.OnLogin', password);
+    mp.events.callRemote('Auth.OnLogin', password);
 
-        if(loginBrowser !== null)
-        {
-            loginBrowser.close();
-            loginBrowser = null;
-        }
+    if(loginWindow != null)
+    {
+        loginWindow.destroy();
+        loginWindow = null;
     }
 })
