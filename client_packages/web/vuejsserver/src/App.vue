059baeaf971fee37bae1a9d29a0e1002056cb2ae
nSystemz <info@nsystemz.de>
2021-08-03 13:40:25 +0200

    Kleinere Anpassungen

---
+++ client_packages/web/vuejsserver/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Notifications ref="notification"/>
+  </div>
+</template>
+
+<script>
+import Notifications from './components/Notifications.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Notifications
+  },
+  mounted() {
+    global.gui.notify = this.$refs.notification;
+  }
+}
+</script>
+
+<style>
+</style>
