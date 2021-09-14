d7b6d4c031b7afa1a9d8361b8eae2c687d7e5d3f
nSystemz <info@nsystemz.de>
2021-09-14 18:47:56 +0200

    vuejsserver aktualisiert

---
+++ vuejsserver/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Notifications ref="notification"/>
+    <Inventory ref="inventory"/>
+  </div>
+</template>
+
+<script>
+import Notifications from './components/Notifications.vue'
+import Inventory from './components/Inventory.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Notifications,
+    Inventory
+  },
+  mounted() {
+    global.gui.notify = this.$refs.notification;
+    global.gui.inventory = this.$refs.inventory;
+  }
+}
+</script>
+
+<style>
+</style>
